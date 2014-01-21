7944ba05edfd7615ece6daf79fd28bcee005da91
amitab <amitab.das@outlook.com>
2014-01-21 21:54:23 +0530

    Notification Service Class

--- lib/timesheet/Notification/Service.php
+++ lib/timesheet/Notification/Service.php
@@ -27,5 +27,31 @@ class Service {
 	
 	// The Cover
 	
-	
+	// write only functions
+    public function createNotification($notificationDetails) {
+        return $this->_dao->createNotification($notificationDetails);
+    }
+    public function deleteNotification($notificationId) {
+        return $this->_dao->deleteNotification($notificationId);
+    }
+    
+    //read functions
+    public function getNotificationById($notificationId) {
+        return $this->_dao->getNotificationById($notificationId);
+    }
+    public function getNotificationFromUser($userId) {
+        return $this->_dao->getNotificationFromUser($userId);
+    }
+    public function getNotificationsToUser($userId) {
+        return $this->_dao->getNotificationsToUser($userId);
+    }
+    public function getNotificationsByPriority($priorityLevel) {
+        return $this->_dao->getNotificationsByPriority($priorityLevel);
+    }
+    public function getUnreadNotificationsForUser($userId) {
+        return $this->_dao->getUnreadNotificationsForUser($userId);
+    }
+    public function getUnreadNotificationCountForUser($userId) {
+        return $this->_dao->getUnreadNotificationCountForUser($userId);
+    }
 }
